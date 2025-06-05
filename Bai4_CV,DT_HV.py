86c2eebb3ea6687ec04eb744af3e6e8609b4d43b
phtrgiang <phtrgiangit@gmail.com>
2025-06-05 23:22:56 +0700

    second file

--- Bai4_CV,DT_HV.py
+++ Bai4_CV,DT_HV.py
@@ -1,4 +1,8 @@
 #4. Tính chu vi và diện tích hình vuông
 a = int(input('a = '))
-print('chu vi hình vuông: ', a*4)
-print('Diện tích hình vuông: ',a*a)
+#print('chu vi hình vuông: ', a*4)
+#print('Diện tích hình vuông: ',a*a)
+CV = a*4
+DT = a*a
+print(CV)
+print(D)
